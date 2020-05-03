ba8f4570495c88d9a99ab76d3f2519b50d2435dd
skalermo <skalermo@gmail.com>
2020-05-03 11:34:32 +0200

    Replace return val -1 with ENOTIMPLEMENTED

--- simplefs/simplefs_api.c
+++ simplefs/simplefs_api.c
@@ -1,37 +1,37 @@
 #include "simplefs_api.h"
 
 int simplefs_open(char *name, int mode) {
-    return -1;
+    return ENOTIMPLEMENTED;
 }
 
 int simplefs_creat(char *name, int mode) {
-    return -1;
+    return ENOTIMPLEMENTED;
 }
 
 int simplefs_read(int fd, char *buf, int len) {
-    return -1;
+    return ENOTIMPLEMENTED;
 }
 
 int simplefs_write(int fd, char *buf, int len) {
-    return -1;
+    return ENOTIMPLEMENTED;
 }
 
 int simplefs_lseek(int fd, int whence, int offset) {
-    return -1;
+    return ENOTIMPLEMENTED;
 }
 
 int simplefs_unlink(char *name) {
-    return -1;
+    return ENOTIMPLEMENTED;
 }
 
 int simplefs_mkdir(char *name) {
-    return -1;
+    return ENOTIMPLEMENTED;
 }
 
 int simplefs_rmdir(char *name) {
-    return -1;
+    return ENOTIMPLEMENTED;
 }
 
 int simplefs_close(int fd) {
-    return -1;
+    return ENOTIMPLEMENTED;
 }
