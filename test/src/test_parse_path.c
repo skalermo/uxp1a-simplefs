25439a8aef67b436d63a09ff6e4296c5ca78abf0
Radoslaw Mierzwa <chalkenteros@gmail.com>
2020-05-05 07:38:27 +0200

    add parse_path tests

---
+++ test/src/test_parse_path.c
@@ -0,0 +1,74 @@
+#include "unity.h"
+#include "simplefs_utils.h"
+#include <stdlib.h>
+
+void setUp(void) {
+
+}
+
+
+void tearDown(void) {
+
+}
+
+void freeStrArr(char*** entries, int dirCount)
+{
+    for(int i = 0; i < dirCount; ++i )
+    {
+        free((*entries)[i]);
+    }
+    free(*entries);
+    *entries = NULL;
+}
+
+void test_dir_count(void)
+{
+    int dirCount = 0;
+    char** entries = NULL;
+
+
+    dirCount = parse_path("/abc/cd/e", &entries);
+    TEST_ASSERT_EQUAL(4, dirCount);
+    freeStrArr(&entries, dirCount);
+
+    //path given as NULL string
+    dirCount = parse_path(NULL, &entries);
+    TEST_ASSERT_EQUAL(EINPATH, dirCount);
+    freeStrArr(&entries, dirCount);
+
+    //path is not absolute
+    dirCount = parse_path("fsxc/fds/frwq/fwq", &entries);
+    TEST_ASSERT_EQUAL(EINPATH, dirCount);
+    freeStrArr(&entries, dirCount);
+
+}
+
+void test_parsed_dirs(void)
+{
+    int dirCount = 0;
+    char** entries = NULL;
+
+    dirCount = parse_path("/abc/cd/e", &entries);
+    TEST_ASSERT_EQUAL_STRING("/", entries[0]);
+    TEST_ASSERT_EQUAL_STRING("abc", entries[1]);
+    TEST_ASSERT_EQUAL_STRING("cd", entries[2]);
+    TEST_ASSERT_EQUAL_STRING("e", entries[3]);
+    freeStrArr(&entries, dirCount);
+
+    dirCount = parse_path(NULL, &entries);
+    TEST_ASSERT_EQUAL(NULL, entries);
+}
+
+
+int main(void) {
+
+
+    UNITY_BEGIN();
+
+
+    RUN_TEST(test_dir_count);
+    RUN_TEST(test_parsed_dirs);
+
+
+    return UNITY_END();
+}
