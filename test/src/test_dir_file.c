e55f82cc286160b590333e7e278dafeac9d655f8
Mateusz Kordowski <mateuszkd67@gmail.com>
2020-05-07 22:16:47 +0200

    Trying to compile.

--- test/src/test_dir_file.c
+++ test/src/test_dir_file.c
@@ -8,16 +8,19 @@
 #ifndef SIMPLEFS_SUPERBLOCK_TEST_C
 #define SIMPLEFS_SUPERBLOCK_TEST_C
 
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/mman.h>
 #include <sys/stat.h> 
 #include <fcntl.h> 
 
 #include "unity.h"
-#include "superblock.h"
-#include "block_links.h"
-#include "inode.h"
-#include "open_files.h"
-#include "dir_file.h"
+#include "memory/superblock.h"
+#include "memory/block_links.h"
+#include "memory/inode.h"
+#include "memory/open_files.h"
+#include "memory/dir_file.h"
 
 void* shm_addr = NULL;
 const uint32_t sizeof_shm = 33554432; //(32 MB)
@@ -25,20 +28,20 @@ const char* shm_name = "shm_test_dir_file";
 
 uint32_t maxOpenFiles = 1024;
 uint32_t maxInodes = UINT16_MAX;
-uint32_t maxFilesystemSize = sizeof_shm;
+uint32_t maxFilesystemSize = 33554432; //(32 MB);
 uint32_t sizeofOneBlock = 1024;
 
 void setUp_dir_file(void){
     // get shm fd
     int fd = shm_open(shm_name, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
-    if (fd == -1) errExit(shm_name);
+    if (fd == -1) exit(EXIT_FAILURE);
 
     // allocate memory in shm 
-    if (ftruncate(fd, sizeof_shm) == -1) errExit("ftruncate");
+    if (ftruncate(fd, sizeof_shm) == -1) exit(EXIT_FAILURE);
 
     // map the object into the caller's address space
     shm_addr = mmap(NULL, sizeof_shm, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (shm_addr == MAP_FAILED) errExit("mmap");
+    if (shm_addr == MAP_FAILED) exit(EXIT_FAILURE);
 
     struct Superblock toSet;
 
@@ -76,11 +79,16 @@ void tearDown_dir_file(void) {
 void dir_file_only_test(void){
     uint32_t blockDirFile;
     struct FS_create_dir_data inodeInfo;
+    char* name1 = "";
+    char* name2 = "DDDD";
+
     inodeInfo.prevoiusDirInode = 0;
     inodeInfo.prevoiusDirInodeLen = 1;
-    inodeInfo.prevoiusDirInodeName = 2;
+    inodeInfo.prevoiusDirInodeName = malloc(61);
+    memcpy(inodeInfo.prevoiusDirInodeName, name1, 1);
     inodeInfo.thisDirInode = 3;
-    inodeInfo.thisDirName = 4;
+    inodeInfo.thisDirName = malloc(61);
+    memcpy(inodeInfo.thisDirName, name2, 5);
     inodeInfo.thisDirNameLen = 5;
 
     char testName[] = "AAC";
@@ -94,10 +102,12 @@ void dir_file_only_test(void){
     TEST_ASSERT_TRUE(fs_save_data_to_dir_entry_inode_number(blockDirFile, 3, 9, shm_addr) == 0);
     TEST_ASSERT_TRUE(fs_get_data_from_dir_entry_name(blockDirFile, 3, recName, &recNameSize, shm_addr) == 0);
     TEST_ASSERT_TRUE(fs_get_data_from_dir_entry_inode_number(blockDirFile, 3, &recInodeNumber, shm_addr) == 0);
-    TEST_ASSERT_TRUE(cmpmem(recName, testName, recNameSize) == 0);
+    TEST_ASSERT_TRUE(memcmp(recName, testName, recNameSize) == 0);
     TEST_ASSERT_TRUE(recInodeNumber == 9);
 
     free(recName);
+    free(inodeInfo.prevoiusDirInodeName);
+    free(inodeInfo.thisDirName);
 }
 
 void dir_file_all_tests(void){
