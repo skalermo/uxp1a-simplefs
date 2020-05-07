b7b853183698a639a6042507e4856bff4fc1f220
rafalb8 <rafalb8@hotmail.com>
2020-05-07 16:18:08 +0300

    Add test cases for set and unset bit

--- test/src/test_bitmap_functions.c
+++ test/src/test_bitmap_functions.c
@@ -8,8 +8,7 @@ void setUp(void) {
 void tearDown(void) {
 }
 
-void test_find_generic(void)
-{
+void test_find_generic(void) {
     uint8_t bitmap[2];
 
     // full bitmap
@@ -29,10 +28,40 @@ void test_find_generic(void)
     TEST_ASSERT_EQUAL(2, find_free_bit(bitmap, 2));
 }
 
+void test_bit_flip(void) {
+    uint8_t bitmap[2];
+
+    // full bitmap
+    bitmap[0] = 0b00000000;
+    bitmap[1] = 0b00000000;
+
+    // Change 0 bit to 1
+    set_bit(bitmap, 0);
+    set_bit(bitmap, 7);
+    set_bit(bitmap, 8);
+    TEST_ASSERT_EQUAL(129, bitmap[0]);
+    TEST_ASSERT_EQUAL(128, bitmap[1]);
+
+    // Change 1 bit to 0
+    unset_bit(bitmap, 0);
+    unset_bit(bitmap, 8);
+    TEST_ASSERT_EQUAL(1, bitmap[0]);
+    TEST_ASSERT_EQUAL(0, bitmap[1]);
+
+    // Try to change 1 bit to 1
+    set_bit(bitmap, 7);
+    TEST_ASSERT_EQUAL(1, bitmap[0]);
+
+    // Try to change 0 bit to 0
+    unset_bit(bitmap, 0);
+    TEST_ASSERT_EQUAL(1, bitmap[0]);
+}
+
 int main(void) {
     UNITY_BEGIN();
 
     RUN_TEST(test_find_generic);
+    RUN_TEST(test_bit_flip);
 
     return UNITY_END();
 }
