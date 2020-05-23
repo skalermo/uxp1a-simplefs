e407d2042bdac32bb2e62e9998abbacac0f33882
skalermo <skalermo@gmail.com>
2020-05-23 13:33:08 +0200

    Rename open flags

--- simplefs/memory/inode.h
+++ simplefs/memory/inode.h
@@ -17,11 +17,11 @@
 
 // Inode mode defines
 
-#define O_RDONLY    0x01
-#define O_WRONLY    0x02
-#define O_RDWR      0x03
+#define RDONLY    0x01
+#define WRONLY    0x02
+#define RDWR      0x03
 
-// O_RDONLY | O_WRONLY == O_RDWR
+// RDONLY | WRONLY == O_RDWR
 
 #define IS_DIR      0x10
 #define IS_FILE     0x30
