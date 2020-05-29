3ade5c1cda72a021e3047ed15d8c9443fa691330
skalermo <skalermo@gmail.com>
2020-05-29 21:24:36 +0200

    Update test

--- test/src/test_mkdir.c
+++ test/src/test_mkdir.c
@@ -22,7 +22,6 @@ void make_one_dir() {
 
     int ret_value = simplefs_mkdir("/a");
     inode_count = fs_get_used_inodes(ptr);
-    printf("%p\n", ptr);
     TEST_ASSERT_EQUAL(3, inode_count);
     TEST_ASSERT_EQUAL(0, ret_value);
 
@@ -33,7 +32,6 @@ void make_three_dirs() {
     // get_ptr_to_fs creates fs
     void *ptr = get_ptr_to_fs();
     uint16_t inode_count = fs_get_used_inodes(ptr);
-    printf("%p\n", ptr);
     TEST_ASSERT_EQUAL(2, inode_count);
 
     int ret_value = simplefs_mkdir("/a");
@@ -41,10 +39,10 @@ void make_three_dirs() {
     TEST_ASSERT_EQUAL(0, ret_value); // <- ret_value == -13
     TEST_ASSERT_EQUAL(3, inode_count);
 
-//    ret_value = simplefs_mkdir("/b");
-//    inode_count = fs_get_used_inodes(ptr);
-//    TEST_ASSERT_EQUAL(4, inode_count);
-//    TEST_ASSERT_EQUAL(0, ret_value);
+    ret_value = simplefs_mkdir("/b");
+    inode_count = fs_get_used_inodes(ptr);
+    TEST_ASSERT_EQUAL(4, inode_count);
+    TEST_ASSERT_EQUAL(0, ret_value);
 //
 //    ret_value = simplefs_mkdir("/c");
 //    inode_count = fs_get_used_inodes(ptr);
