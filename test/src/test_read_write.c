ad89f4772950f0b082d3523401599ea82f32ec21
Radoslaw Mierzwa <chalkenteros@gmail.com>
2020-06-01 21:25:09 +0200

    add read write tests

--- test/src/test_read_write.c
+++ test/src/test_read_write.c
@@ -37,26 +37,83 @@ void successfully_write_file_and_return_written_bytes_count(){
     TEST_ASSERT_EQUAL(100, result);
 }
 
+void successfully_write_file_twice(){
+    char buf_1[10] = "123456789";
+    char buf_2[4] = "123";
+    int fd = simplefs_creat("/write_mode_count_twice", FS_WRITE);
+    int result_1 = simplefs_write(fd, &buf_1, sizeof(buf_1));
+    int result_2 = simplefs_write(fd, &buf_2, sizeof(buf_2));
+    TEST_ASSERT_EQUAL(10, result_1);
+    TEST_ASSERT_EQUAL(4, result_2);
+}
+
+
+
 void successfully_read_file_and_return_read_bytes_count(){
     char buf[100] = "123456789";
     char buf_read[50];
-    int fd = simplefs_creat("/read_mode_count", FS_WRITE);
+    int fd = simplefs_creat("/read_write_count", FS_WRITE);
     int result = simplefs_write(fd, &buf, sizeof(buf));
-    int fd_open = simplefs_open("/read_mode_count", FS_READ);
+    int fd_open = simplefs_open("/read_write_count", FS_READ);
     int read_result = simplefs_read(fd_open, &buf_read, sizeof(buf_read));
     TEST_ASSERT(!strcmp(buf_read, "123456789"));
     TEST_ASSERT_EQUAL(50, read_result);
 }
 
+void read_file_twice_gives_correct_result(){
+    char buf[100] = "123456789";
+    char buf_read_1[100];
+    char buf_read_2[100];
+    int fd = simplefs_creat("/read_mode_twice", FS_WRITE);
+    int result = simplefs_write(fd, &buf, sizeof(buf));
+    int fd_open = simplefs_open("/read_mode_twice", FS_READ);
+    int read_result_1 = simplefs_read(fd_open, &buf_read_1, 100);
+    int read_result_2 = simplefs_read(fd_open, &buf_read_2, 100);
+    TEST_ASSERT(!strcmp(buf_read_1, "123456789"));
+    TEST_ASSERT(!strcmp(buf_read_2, ""));
+    TEST_ASSERT_EQUAL(read_result_1, read_result_2);
+}
+
 void read_file_more_bytes_than_buffer_size(){
     char buf[100] = "123456789";
     char buf_read[50];
-    int fd = simplefs_creat("/read_mode_count", FS_WRITE);
+    int fd = simplefs_creat("/read_mode_buf", FS_WRITE);
     int result = simplefs_write(fd, &buf, sizeof(buf));
-    int fd_open = simplefs_open("/read_mode_count", FS_READ);
+    int fd_open = simplefs_open("/read_mode_buf", FS_READ);
     int read_result = simplefs_read(fd_open, &buf_read, 100);
     TEST_ASSERT(!strcmp(buf_read, "123456789"));
     TEST_ASSERT_EQUAL(50, read_result);
+}
+
+
+void successfully_write_file_twice_and_read_different_content(){
+    char buf_1[10] = "123456789";
+    char buf_2[4] = "123";
+    char buf_read_1[100];
+    char buf_read_2[100];
+    int fd = simplefs_creat("/write_mode_twice", FS_WRITE);
+    int fd_open = simplefs_open("/write_mode_twice", FS_READ);
+    simplefs_write(fd, &buf_1, sizeof(buf_1));
+    simplefs_read(fd_open, &buf_read_1, 10);
+    TEST_ASSERT(!strcmp(buf_read_1, "123456789"));
+    simplefs_write(fd, &buf_2, sizeof(buf_2));
+    simplefs_read(fd_open, &buf_read_2, 4);
+    TEST_ASSERT(!strcmp(buf_read_2, "123"));
+}
+
+void write_read_file_multiple_blocks(){
+    char buf[601] ="0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+                   "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+                   "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+                   "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+                   "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+                   "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"; //601B
+    char buf_read[601];
+    int fd = simplefs_creat("/multiple_blocks", FS_WRITE);
+    int result = simplefs_write(fd, &buf, 601);
+    int fd_open = simplefs_open("/multiple_blocks", FS_READ);
+    int read_result = simplefs_read(fd_open, &buf_read, 601);
+    TEST_ASSERT(!strcmp(buf_read, buf));
 }
 
 
@@ -69,6 +126,10 @@ int main(void) {
     RUN_TEST(successfully_write_file_and_return_written_bytes_count);
     RUN_TEST(successfully_read_file_and_return_read_bytes_count);
     RUN_TEST(read_file_more_bytes_than_buffer_size);
+    RUN_TEST(read_file_twice_gives_correct_result);
+    RUN_TEST(successfully_write_file_twice_and_read_different_content);
+    RUN_TEST(successfully_write_file_twice);
+    RUN_TEST(write_read_file_multiple_blocks);
 
     return UNITY_END();
 }
